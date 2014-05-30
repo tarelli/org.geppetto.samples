ede2bf8bd19cf26196751166b3c1b333fd94125f
Boris Marin <borismarin@gmail.com>
2014-05-30 20:11:21 +0100

    initial potulum example commit

---
+++ LEMS/Potulum/Pot_Geppetto_Script.js
@@ -0,0 +1,20 @@
+Simulation.addWatchLists([{name:"pendvars",variablePaths:["pendulum.mechanical.theta1"]}]);
+Simulation.startWatch();
+Simulation.start();
+
+
+//Adding Plot 1
+
+G.addWidget(Widgets.PLOT);
+
+Plot1.setName("Theta 1");
+
+options = {yaxis:{min:-1,max:1},xaxis:{min:0,max:400,show:false}};
+
+Plot1.setOptions(options);
+Plot1.setPosition(113, 90);
+Plot1.setSize(230,445)
+Plot1.plotData("pendulum.mechanical.theta1");
+
+
+//window.setTimeout(function(){Simulation.addBrightnessFunction("hhcell.electrical", "hhcell.electrical.hhpop[0].v", function(x){return (x+0.06)/0.06;})},500);
