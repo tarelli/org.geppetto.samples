b30ae50b63d75edf90d0a89bcb21f0f4e9a7964b
Boris Marin <borismarin@gmail.com>
2014-06-02 12:34:40 +0100

    trying to understand geppetto aspects

--- LEMS/Potulum/Pot_Geppetto_Script.js
+++ LEMS/Potulum/Pot_Geppetto_Script.js
@@ -17,4 +17,5 @@ Plot1.setPosition(15,560)
 Plot1.plotData("pendulum.mechanical.theta1");
 
 
-window.setTimeout(function(){Simulation.mapWatchToRepresentation("pendulum.mechanical", "pendulum.mechanical.theta1", function(a, b){console.log('taka', a, b)})},500);
+// window.setTimeout(function(){Simulation.mapWatchToRepresentation("pendulum.mechanical", "pendulum.mechanical.theta1", function(a, b){console.log('taka', a, b)})},500);
+window.setTimeout(function(){Simulation.mapWatchToRepresentation("pendulum.mechanical", "pendulum.mechanical.theta1", function(a, b){},500);
