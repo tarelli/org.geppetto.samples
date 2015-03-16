ea3fbe4fca982857fcd74ec8386184fb44856076
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2015-03-16 15:18:13 +0000

    Update Pot_Geppetto_Script.js

--- LEMS/Potulum/Pot_Geppetto_Script.js
+++ LEMS/Potulum/Pot_Geppetto_Script.js
@@ -14,7 +14,7 @@ options = {yaxis:{min:-2,max:2},xaxis:{min:0,max:10000,show:false}};
 Plot1.setOptions(options);
 Plot1.setSize(190, 730);
 Plot1.setPosition(15,560)
-Plot1.plotData("pendulum.mechanical.theta1");
+Plot1.plotData("pendulum.mechanical.SimulationTree.theta1");
 
 
 window.setTimeout(function(){Simulation.mapWatchToRepresentation("pendulum.mechanical", "pendulum.mechanical.SimulationTree.theta1", function(a, b){})},500);
